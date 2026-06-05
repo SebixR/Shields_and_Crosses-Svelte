4efa71fc7ab78b4ad21ac98052a639d8d9ccac54
SebixR <sebarog@gmail.com>
2026-06-05 10:48:42 +0200

    Characters can be deleted

--- src/lib/CharacterService.svelte.ts
+++ src/lib/CharacterService.svelte.ts
@@ -1,4 +1,4 @@
-import { createCharacter, getAllCharacters } from './api/character';
+import { db } from '../db';
 import type { Character, CharacterRecord } from './types/character';
 
 class CharacterService {
@@ -17,6 +17,45 @@ class CharacterService {
 
 		await this.init();
 	}
+
+	async delete(id: number) {
+		await deleteCharacter(id);
+
+		await this.init();
+	}
 }
 
 export const characterService = new CharacterService();
+
+export async function createCharacter(character: Character) {
+	try {
+		await db.characters.add({
+			...character,
+			wins: 0,
+			losses: 0
+		});
+	} catch (error) {
+		console.error('Failed to create the character ' + character.name, error);
+		throw error;
+	}
+}
+
+export async function getAllCharacters(page?: number, size?: number): Promise<CharacterRecord[]> {
+	try {
+		const collection = db.characters.offset((page ?? 0) * (size ?? 0)).limit(size ?? 10);
+
+		return await collection.toArray();
+	} catch (error) {
+		console.error('Failed to get all characters', error);
+		throw error;
+	}
+}
+
+export async function deleteCharacter(id: number) {
+	try {
+		await db.characters.delete(id);
+	} catch (error) {
+		console.error('Failed to delete character with id: ' + id, error);
+		throw error;
+	}
+}
--- src/lib/components/CharactersTable.svelte
+++ src/lib/components/CharactersTable.svelte
@@ -4,12 +4,13 @@
 	import { classLabels } from '$lib/types/class';
 	import { Button } from './ui/button';
 	import { BicepsFlexed, Brain, PenIcon, SportShoe, Trash2Icon } from '@lucide/svelte';
+	import * as Dialog from '$lib/components/ui/dialog/index.js';
 </script>
 
 <Table.Root>
 	<Table.Header>
 		<Table.Row>
-			<Table.Head class="w-25">Name</Table.Head>
+			<Table.Head class="w-36">Name</Table.Head>
 			<Table.Head>Class</Table.Head>
 			<Table.Head><BicepsFlexed class="m-auto" size="20" /></Table.Head>
 			<Table.Head><SportShoe class="m-auto" size="20" /></Table.Head>
@@ -27,9 +28,38 @@
 					<Table.Cell class="text-center">{character.speed}</Table.Cell>
 					<Table.Cell class="text-center">{character.intelligence}</Table.Cell>
 					<Table.Cell class="text-center">{character.wins}/{character.losses}</Table.Cell>
-					<Table.Cell class="text-end"
-						><Button class="me-1" variant="outline" size="icon"><PenIcon /></Button>
-						<Button variant="destructive" size="icon"><Trash2Icon /></Button>
+					<Table.Cell class="text-end">
+						<Dialog.Root>
+							<Dialog.Trigger>
+								<Button variant="destructive" size="icon">
+									<Trash2Icon />
+								</Button>
+							</Dialog.Trigger>
+							<Dialog.Content>
+								<Dialog.Header>
+									<Dialog.Title>Delete character</Dialog.Title>
+									<Dialog.Description
+										>Are you sure you want to delete {character.name}?</Dialog.Description
+									>
+								</Dialog.Header>
+								<div class="flex flex-row justify-between">
+									<Dialog.Close>
+										<Button
+											onclick={() => characterService.delete(character.id)}
+											variant="destructive">Delete {character.name}</Button
+										>
+									</Dialog.Close>
+
+									<Dialog.Close>
+										<Button variant="outline">Cancel</Button>
+									</Dialog.Close>
+								</div>
+							</Dialog.Content>
+						</Dialog.Root>
+
+						<Button class="me-1" variant="outline" size="icon">
+							<PenIcon />
+						</Button>
 					</Table.Cell>
 				</Table.Row>
 			{/each}
